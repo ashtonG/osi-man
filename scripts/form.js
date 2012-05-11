9ad54ae8938aea0468d2fd91986adb4a94bf3f9a
Ash G <gpotatop+GITHUB@gmail.com>
2012-05-11 13:13:23 -0400

    basic html and javascript class

---
+++ scripts/form.js
@@ -0,0 +1,21 @@
+function calEvent(title, description, date, icon){
+	this.data = {
+		"title" : title,
+		"details" : description,
+		"location" : "not implemented",
+		"when" : [
+		{
+			"start": date,
+			"end": "not implemented"
+		}
+		],
+		"webContent": {
+			"icon" : "not implemented",
+			"title": title,
+			"url" : "not implemented"
+		}
+	}
+}
+function createEvent(){
+
+}
